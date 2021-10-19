1e3f1be5c86dd7de2ff37c5987e17a52414c2d6b
Carl-Erik Bergström <calle.bergstrom@live.se>
2021-10-19 12:12:47 +0200

    Linux

--- keyboards/lily58/keymaps/callebstrom/keymap.c
+++ keyboards/lily58/keymaps/callebstrom/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   Ö  |  Ä   |
  * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
- * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  | RAlt |
+ * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI | ESC  | /SPACE  /       \Enter \  |RAISE |BackSP| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                       KC_7,    KC_8,    KC_9,    KC_0,    SE_PLUS, KC_PLUS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG, \
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA, SE_ADIA, \
-  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, SE_LESS_MAC,  SE_GRTR_MAC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, KC_LGUI, KC_ESC, KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
+  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, SE_LABK,  KC_GT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                        KC_LALT, KC_LGUI, KC_ESC, KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RALT \
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_1,   KC_2,     KC_3,    KC_4,    KC_5,    KC_6,                        KC_7,    KC_8,    KC_9,    KC_0,    SE_PLUS, SE_DIAE, \
   KC_LSFT, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, SE_QUOT, \
-  KC_LCTRL, SE_LESS_MAC, XXXXXXX, XXXXXXX, KC_VOLD, XXXXXXX,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, SE_TILD, \
+  KC_LCTRL, KC_LT, XXXXXXX, XXXXXXX, KC_VOLD, XXXXXXX,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, SE_TILD, \
                         KC_LALT, KC_LGUI, KC_DELETE, KC_BSPC, KC_ENT, MO(_RAISE), KC_DELETE, KC_RGUI \
 ),
 /* ADJUST
